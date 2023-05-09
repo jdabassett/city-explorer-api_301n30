ddd34f4358484c2f54e4cbb3dc44361dd6c178b4
jdabassett <jacobbassett@gmail.com>
2023-05-09 17:03:18 -0600

    lab08-1 finished

--- server.js
+++ server.js
@@ -4,8 +4,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const data = require('./data/weather.json');
+const axios = require('axios');
+
+
+//import global variables
 const port = process.env.PORT || 3001;
+const SERVER = process.env.SERVER;
+const WEATHERKEY_TOKEN = process.env.WEATHERKEY;
 
 //set up server
 const app = express();
@@ -21,32 +26,63 @@ class Forecast {
   constructor(cityObject) {
     if (typeof cityObject !== 'object') throw new Error('List type error');
     this.cityObject = cityObject || {};
-    this.forecastArray = cityObject.data || null;
+    this.forecastArray = cityObject.data || [];
+    this.weekdayArray = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    this.monthArray = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   }
+  //change the format of the day
+  formateDay (dateStr) {
+    let dateObject = new Date(dateStr);
+    let weekday = this.weekdayArray[dateObject.getDay()];
+    let monthDay = dateObject.getDate();
+    let month = this.monthArray[dateObject.getMonth()];
+    // let following = 
+    //   if(monthDay===1){return 'st'};
+    //   else if(monthDay===2){return 'nd'};
+    //   else if(monthDay===3){return 'st'};
+    //   else {return 'th'};
+    // };
+
+    return `${weekday}, ${month} ${monthDay}`;
+  }
+
+  //convert celsius to fahrenheit
+  convertCtF (celsius) {
+    let fahrenheit = Math.round((parseFloat(celsius)*(9/5))+32);
+    return fahrenheit;
+  }
+
+
+
+
   // method to return forecast array
   getItems() {
+    // console.log('+++++++++++++++++++++++++++++', this.forecastArray);
     return this.forecastArray.map(item => ({
-      name: item.datetime,
-      description: `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description.toLowerCase()}`
+      name: this.formateDay(item.datetime),
+      description_C: `Low of ${Math.round(item.low_temp)}°C, high of ${Math.round(item.high_temp)}°C with ${item.weather.description.toLowerCase()}`,
+      description_F: `Low of ${this.convertCtF(item.low_temp)}°F, high of ${this.convertCtF(item.high_temp)}°F with ${item.weather.description.toLowerCase()}`
     }));
   }
 }
 
-
+// http://api.weatherbit.io/v2.0/forecast/daily?key=7a4a564fd2ba43eaa78e02ace133c6d6&lat=47.6038321&lon=-122.330062
 // get weather router
 app.get('/weather', async (req,res,next)=>{
-  const searchQuery  = `${req.query.searchQuery}`.toLowerCase();
+  const {lat,lon} = req.query;
+  let weatherQuery = {
+    url:`http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHERKEY_TOKEN}&lat=${lat}&lon=${lon}`,
+    method:'GET'};
 
   try {
-    let record = await data.find(item =>item.city_name.toLowerCase()===searchQuery);
-    // let record = null;
-    if (record === null || record===undefined){
-      // throw new Error('List type error');
-      throw new Error('this is my message');
-      // return res.status(404).json({message:'Couldn\'t find record'});
-    }
+
+    let weatherResponse = require('./data/seattle_weather.json');
+    // let weatherResponse = await axios(weatherQuery);
+    // console.log(weatherResponse);
+
+  
     // generate forecast object
-    let forecastObject = new Forecast(record);
+    let forecastObject = new Forecast(weatherResponse);
     let forecastArray = forecastObject.getItems();
     //return get request of forecast array
     return res.status(200).send(forecastArray);
