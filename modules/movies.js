9a60e930472454223a010010c648016d9f15aa5e
jdabassett <jacobbassett@gmail.com>
2023-05-15 22:30:38 -0600

    mvp lab10

--- modules/movies.js
+++ modules/movies.js
@@ -3,32 +3,54 @@
 require('dotenv').config();
 const axios = require('axios');
 const TMDBKEY_TOKEN = process.env.TMDBKEY;
-const {MoviesFormated} = require('./class.js');
+const cache = require('./cache.js');
 
 
 async function moviesRequest (req,res,next) {
   const searchQuery = req.query.searchQuery;
+  let key = `movies?searchQuery=${searchQuery}`;
 
   let moviesQuery = {
     url:`https://api.themoviedb.org/3/search/movie?api_key=${TMDBKEY_TOKEN}&query=${searchQuery}`,
     method:'GET'};
 
-  axios(moviesQuery)
-    .then(res => new MoviesFormated(res))
-    .then(moviesObject => moviesObject.getItems().slice(0,20))
-    // .then(moviesArray => res.status(404).json({message:'movies error'}))
-    .then(moviesArray => res.status(200).send(moviesArray))
-    .catch(error => next(error));
-  
-  // old way of doing it
-  // try {
-  //   let moviesResponse = await axios(moviesQuery );
-  //   let moviesObject = new MoviesFormated(moviesResponse);
-  //   let moviesArray = moviesObject.getItems().slice(0,20);
-  //   return res.status(200).send(moviesArray);
-  // } catch (error){
-  //   next(error);
-  // }
+  if (cache[key] && (Date.now()-cache[key].timestamp<10000000)){
+    console.log('found in cache');
+    res.status(200).send(cache[key].data);
+  } else {
+    axios(moviesQuery)
+      .then(res => new MoviesFormated(res))
+      .then(moviesObject => moviesObject.getItems().slice(0,20))
+      // .then(moviesArray => res.status(404).json({message:'movies error'}))
+      .then(moviesArray => {
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+        cache[key].data = moviesArray;
+        console.log('movies-',cache[key].timestamp);
+        res.status(200).send(moviesArray);
+      })
+      .catch(error => next(error));
+  }
+}
+
+class MoviesFormated {
+  constructor(movies) {
+    if (typeof movies !== 'object') throw new Error('List type error');
+    this.moviesArray = movies.data.results.sort((a,b)=>b.popularity-a.popularity) || [];
+  }
+
+  getItems() {
+    // console.log('+++++++++++++++++++++++++++++', this.moviesArray);
+    return this.moviesArray.map(item => ({
+      title:item.title,
+      overview:item.overview,
+      average_votes:item.vote_average,
+      total_votes:item.vote_count,
+      image_url:`https://image.tmdb.org/t/p/w500/${item.poster_path}`,
+      popularity:item.popularity,
+      released_on:item.release_date,
+    }));
+  }
 }
 
 module.exports = moviesRequest;
--- modules/weather.js
+++ modules/weather.js
@@ -4,32 +4,73 @@
 require('dotenv').config();
 const axios = require('axios');
 const WEATHERKEY_TOKEN = process.env.WEATHERKEY;
-const {Forecast} = require('./class.js');
-
+const cache = require('./cache.js');
 
 
 function weatherRequest(req,res,next){
   const {lat,lon} = req.query;
+  let key = `weather?lat=${lat}&lon=${lon}`;
   let weatherQuery = {
     url:`http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHERKEY_TOKEN}&lat=${lat}&lon=${lon}`,
     method:'GET'};
 
-  axios(weatherQuery)
-    .then(res => new Forecast(res))
-    .then(forecastObject => forecastObject.getItems())
-    // .then(forecastArray => res.status(404).json({message:'movies error'}))
-    .then(forecastArray => res.status(200).send(forecastArray))
-    .catch(err => next(err));
+  if (cache[key] && (Date.now()-cache[key].timestamp)<10000000){
+    // console.log('found cache');
+    res.status(200).send(cache[key].data);
+  } else{
+    // console.log('requested new, added to cache');
+    axios(weatherQuery)
+      .then(res => new Forecast(res))
+      .then(forecastObject => forecastObject.getItems())
+      // .then(forecastArray => res.status(404).json({message:'movies error'}))
+      .then(forecastArray => {
+        // throw new Error('this is my testing error message');
+        cache[key]={};
+        cache[key].timestamp=Date.now();
+        cache[key].data=forecastArray;
+        console.log('weather-',cache[key].timestamp);
+        res.status(200).send(forecastArray);
+      })
+      .catch(err => next(err));
+  }
+}
 
-  // old way of doing it
-  // try {
-  //   let weatherResponse = await axios(weatherQuery);
-  //   let forecastObject = new Forecast(weatherResponse);
-  //   let forecastArray = forecastObject.getItems();
-  //   return res.status(200).send(forecastArray);
-  // } catch (error){
-  //   next(error);
-  // }
+
+class Forecast {
+  constructor(cityObject) {
+    if (typeof cityObject !== 'object') throw new Error('List type error');
+    this.cityObject = cityObject || {};
+    this.forecastArray = cityObject.data.data || [];
+    this.weekdayArray = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+    this.monthArray = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  }
+  //change the format of the day
+  formateDay (dateStr) {
+    let dateObject = new Date(dateStr);
+    let weekday = this.weekdayArray[dateObject.getDay()];
+    let monthDay = dateObject.getDate();
+    let month = this.monthArray[dateObject.getMonth()];
+    return `${weekday}, ${month} ${monthDay}`;
+  }
+
+  //convert celsius to fahrenheit
+  convertCtF (celsius) {
+    let fahrenheit = Math.round((parseFloat(celsius)*(9/5))+32);
+    return fahrenheit;
+  }
+
+  // method to return forecast array
+  getItems() {
+    return this.forecastArray.map(item => ({
+      name: this.formateDay(item.datetime),
+      description_C: `Low of ${Math.round(item.low_temp)}°C, 
+                      high of ${Math.round(item.high_temp)}°C
+                      with ${item.weather.description.toLowerCase()}`,
+      description_F: `Low of ${this.convertCtF(item.low_temp)}°F,
+                      high of ${this.convertCtF(item.high_temp)}°F
+                      with ${item.weather.description.toLowerCase()}`
+    }));
+  }
 }
 
 
